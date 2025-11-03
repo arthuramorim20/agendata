5405985ae061b7d81840524b04561d07b8d54a1a
Arthur Evaristo <aer47186@gmail.com>
2025-11-03 16:28:47 -0400

    ..

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,7 @@
+
+import { ServicesService } from './services/services.service';
+import { ControllersController } from './controllers/controllers.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { ActionsController } from './actions/actions.controller';
+import { AuthController } from './auth/auth.controller';
